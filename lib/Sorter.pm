1ee88167d1edce5b88d7f2735106d1c18287395f
Abe Takashi <abe.univ+github@gmail.com>
2010-07-18 01:43:06 +0900

    make new(), set_values(), get_values()

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,25 @@
-# implement this
+package Sorter;
+
+use strict;
+
+sub new {
+    my $class = shift;
+    return bless([], $class);
+}
+
+sub set_values {
+    my $self = shift;
+    @$self = @_;
+    return;
+}
+
+sub get_values {
+    my $self = shift;
+    return @$self;
+}
+
+sub sort {
+    
+}
+
+1;
