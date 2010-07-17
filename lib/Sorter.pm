0e5cd05264f19d8ad8b291d7ef45ba63b928f11a
Abe Takashi <abe.univ+github@gmail.com>
2010-07-18 03:18:02 +0900

    すごい冗長だった

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,7 +1,6 @@
 package Sorter;
 
 use strict;
-use Carp;
 
 sub new {
     my $class = shift;
@@ -23,60 +22,29 @@ sub sort {
     my $self  = shift;
     my $left  = @_ ? shift : 0;
     my $right = @_ ? shift : $#{$self};
-    my $pivot = $self->_select_pivot($left, $right);
+    my $pivot = $self->_pivot($left, $right);
 
     return if( $right - $left < 1 );
-
-    # print $pivot, "@";
-    # print join(',', $self->get_values), "\n";
-    # print join(',', @$self[$left..$right]);
-    # <>;
 
     my $i = $left;
     my $j = $right;
     for(;;){
-        my $l = $self->_search_to_right($i, $right, $pivot);
-        my $r = $self->_search_to_left($j, $left, $pivot);
-        if( $l < $r ){
-            my $tmp = $self->[$l];
-            $self->[$l] = $self->[$r];
-            $self->[$r] = $tmp;
-            $i = $l + 1;
-            $j = $r - 1;
+        $i++ while($self->[$i] < $pivot);
+        $j-- while($self->[$j] > $pivot);
+        if( $i < $j ){
+            my $tmp = $self->[$i];
+            $self->[$i] = $self->[$j];
+            $self->[$j] = $tmp;
+            $i++; $j--;
             next;
         }
-        $self->sort($left, $l-1);
-        $self->sort($l, $right);
+        $self->sort($left, $i-1);
+        $self->sort($i, $right);
         last;
     }
 }
 
-sub _search_to_right {
-    my $self  = shift;
-    my $left  = shift;
-    my $right = shift;
-    my $pivot = shift;
-
-    for my $i ($left..$right){
-        return $i if( $self->[$i] >= $pivot );
-    }
-    croak("何かおかしい");
-}
-
-sub _search_to_left {
-    my $self  = shift;
-    my $right = shift;
-    my $left  = shift;
-    my $pivot = shift;
-
-    for my $i (0..$right - $left){
-        my $j = $right - $i;
-        return $j if( $self->[$j] <= $pivot );
-    }
-    croak("何かおかしい");
-}
-
-sub _select_pivot {
+sub _pivot {
     my $self = shift;
     my $left = shift;
     my $right = shift;
